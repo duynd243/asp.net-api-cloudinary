d6f77857e5c71ae279c8326c7446ccbf0d0ef623
duyndse <duyndse@gmail.com>
2022-11-25 14:13:59 +0700

    init

---
+++ cloudinary-api/Controllers/UploadController.cs
@@ -0,0 +1,35 @@
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cloudinary_api.Controllers;
+
+[ApiController]
+public class UploadController : ControllerBase
+{
+    private readonly IConfiguration _configuration;
+
+    public UploadController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    [HttpPost]
+    [Route("api/upload")]
+    public ActionResult Upload(IFormFile file)
+    {
+        var cloudinary = new Cloudinary(new Account(
+            cloud: _configuration.GetSection("Cloudinary:CloudName").Value,
+            apiKey: _configuration.GetSection("Cloudinary:ApiKey").Value,
+            apiSecret: _configuration.GetSection("Cloudinary:ApiSecret").Value
+        ));
+
+        var uploadParams = new ImageUploadParams()
+        {
+            File = new FileDescription(file.FileName, file.OpenReadStream())
+        };
+        var uploadResult = cloudinary.Upload(uploadParams);
+
+        return Ok(new {uploadResult.Url});
+    }
+}
